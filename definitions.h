5d4abe4d69ae99a10dbbc75d70a1e3b5cf7d0a2d
lucadev23 <luca.mer13@gmail.com>
2022-02-04 02:26:32 +0100

    Add files via upload

---
+++ definitions.h
@@ -0,0 +1,10 @@
+#ifndef __DEF_JPEG__
+#define __DEF_JPEG__
+#endif
+
+#define FF 0b11111111
+#define D9 0b11011001
+#define D8 0b11011000
+#define E0 0b11100000
+#define B00 0b00000000
+#define B10 0b00010000
